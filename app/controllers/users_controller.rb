d0be7d9e37eed58d8713f393ee50728e01ed902b
En1gma488 <eigmo@mail.ru>
2018-05-22 22:00:58 +0300

    2 working btn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < Clearance::UsersController
+	def index
+		@users = User.all
+	end	
+
+	def show
+ 	@user = User.find(params[:id])
+	end
+
 	private
 
 	def user_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180517201225) do
+ActiveRecord::Schema.define(version: 20180522182346) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
