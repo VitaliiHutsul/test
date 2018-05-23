1ca3334996191f7d07c7fea2b94ad19f93acb978
En1gma488 <eigmo@mail.ru>
2018-05-23 18:16:38 +0300

    setting + top bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < Clearance::UsersController
+	
 	def index
 		@users = User.all
 	end	
@@ -7,9 +8,22 @@ class UsersController < Clearance::UsersController
  	@user = User.find(params[:id])
 	end
 
+	def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if
+    @user.update_attributes(user_params)
+    else
+    render :edit
+    end
+  end  
+	
 	private
 
 	def user_params
-		params.require(:users).permit(:first_name, :second_name, :email, :password)
+		params.require(:user).permit(:first_name, :second_name, :email, :password)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  
+  get 'posts/:id/edit', to: 'posts#edit'
+  post 'posts/:id/edit', to: 'posts#update'
   resources :users, controller: 'users'
 end
